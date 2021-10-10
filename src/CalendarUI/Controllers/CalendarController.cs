fc326a1753e8133ef6c4dc1ecedcf635b463d8ee
bakhtiary <Bakhtiary.amir@hotmail.com>
2021-10-10 23:18:44 +0330

    Add project files.

---
+++ src/CalendarUI/Controllers/CalendarController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using CalendarUI.Models;
+using CalendarUI.Service.Query;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarUI.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+        public CalendarController(IMapper mapper, IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        [Route("calendar")]
+        public IActionResult Index()
+        {
+            var monthModel = new List<MonthViewModel>();
+            monthModel = Enumerable.Range(1, 12)
+                .Select(i => new MonthViewModel
+                {
+                    Id = i,
+                    Name = DateTimeFormatInfo.CurrentInfo.GetMonthName(i).Substring(0, 3)
+                })
+                .ToList();
+
+            return View(monthModel);
+        }
+
+        [Route("appointments/{month}")]
+        public async Task<IActionResult> GetAppointments(int month)
+        {
+            var appointments = await _mediator.Send(
+                new GetAppointmentsByMonthQuery
+                {
+                    Month = month
+                });
+            return PartialView("_Appointments", appointments);
+        }
+
+        [Route("appointmentdetail/{id}")]
+        public async Task<IActionResult> GetAppointmentDetail(Guid id)
+        {
+            var appointment = await _mediator.Send(
+                new GetAppointmentByIdQuery
+                {
+                    Id = id
+                });
+            return PartialView("_AppointmentDetail", appointment);
+        }
+    }
+}
---
+++ src/CalendarUI/Extentions/ServiceCollectionExtensions.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using CalendarUI.Data.Database;
+using CalendarUI.Data.Repository;
+using CalendarUI.Infrastructure.AutoMapper;
+using CalendarUI.Models;
+using CalendarUI.Service.Query;
+using CalendarUI.Validators;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace CalendarUI.Extentions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCustomizeControllers(this IServiceCollection services)
+        {
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation()
+                .SetCompatibilityVersion(CompatibilityVersion.Latest);
+            return services;
+        }
+
+        public static IServiceCollection AddCustomizeService(this IServiceCollection services)
+        {
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+            services.AddTransient<IValidator<CreateAppointmentModel>, CreateAppointmentModelValidator>();
+            services.AddTransient<IValidator<UpdateAppointmentModel>, UpdateAppointmentModelValidator>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomizedDataStore(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDbContext<CalendarContext>(options => options.UseSqlServer(configuration.GetConnectionString("DatabaseConnection"),
+                        b =>
+                        {
+                            b.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+                            b.MigrationsHistoryTable($"__{nameof(CalendarContext)}");
+                        }));
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigAutoMapper(this IServiceCollection services)
+        {
+            services.AddTransient(provider => new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            }).CreateMapper());
+            return services;
+        }
+
+        public static IServiceCollection ConfigMediatR(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.Load(typeof(GetAppointmentByIdQuery).GetTypeInfo().Assembly.GetName().Name));
+            return services;
+        }
+
+
+        public static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddHealthChecks()
+                .AddCheck("self", () => HealthCheckResult.Healthy());
+
+            return services;
+        }
+
+        public static void UpdateDatabase(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope();
+            using var calendarContext = serviceScope.ServiceProvider.GetService<CalendarContext>();
+            calendarContext.Database.Migrate();
+        }
+    }
+}
